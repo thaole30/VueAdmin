38834601480d3f1e14d62a5327d70e32b3397608
Lê Phương Thảo <thao.le_phuong@napaglobal.com>
2022-09-12 12:49:11 +0700

    init project

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,76 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view />
-  </div>
+  <v-app>
+    <v-app-bar style="min-height: 56px" app color="white" elevation="2" dark>
+      <v-row no-gutters align="center">
+        <v-col xs="12" sm="12" md="6">
+          <div class="d-flex align-center">
+            <v-img
+              alt="Vuetify Logo"
+              class="shrink mr-2"
+              contain
+              src="https://technext.github.io/elaadmin/images/logo.png"
+              transition="scale-transition"
+              width="160"
+            />
+            <v-btn icon color="#99abb4">
+              <v-icon>mdi-magnify</v-icon>
+            </v-btn>
+          </div>
+        </v-col>
+        <v-col xs="12" sm="12" md="6">
+          <v-row align="center" justify="end" color="grey lighten-2" flat tile>
+            <v-btn icon color="#99abb4">
+              <v-icon>mdi-magnify</v-icon>
+            </v-btn>
+            <v-badge color="red" content="6" offset-x="20" offset-y="25">
+              <v-btn icon color="#99abb4">
+                <v-icon>mdi-bell</v-icon>
+              </v-btn>
+            </v-badge>
+            <v-badge color="blue" content="6" offset-x="20" offset-y="25">
+              <v-btn icon color="#99abb4">
+                <v-icon>mdi-email</v-icon>
+              </v-btn>
+            </v-badge>
+
+            <v-badge
+              bordered
+              bottom
+              color="green accent-4"
+              dot
+              offset-x="10"
+              offset-y="10"
+              style="margin-right: 20px; margin-left: 10px"
+            >
+              <v-avatar size="40">
+                <v-img
+                  src="https://cdn.vuetifyjs.com/images/lists/2.jpg"
+                ></v-img>
+              </v-avatar>
+            </v-badge>
+          </v-row>
+        </v-col>
+      </v-row>
+    </v-app-bar>
+
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
 </template>
 
+<script>
+export default {
+  name: "App",
+
+  data: () => ({
+    //
+  }),
+};
+</script>
+
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+.v-app-bar {
+  min-height: 56px !important;
 }
 </style>
---
+++ src/views/DemoVuetify.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-app>
+    <div class="ma-4">
+      <v-card class="d-flex flex-row mb-6" color="grey lighten-4" flat tile>
+        <v-card v-for="n in 3" :key="n" class="pa-2" outlined tile>
+          Flex item {{ n }}
+        </v-card>
+      </v-card>
+      <v-card class="d-flex flex-row-reverse" color="green lighten-4" flat tile>
+        <v-card v-for="n in 3" :key="n" class="pa-2" outlined tile>
+          Flex item {{ n }}
+        </v-card>
+      </v-card>
+    </div>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "DemoVuetify",
+};
+</script>

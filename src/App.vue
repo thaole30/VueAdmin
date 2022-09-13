72fc404805be4b35860fdf8b93b6f0a6a46e714e
thaole30 <thaolee3005@gmail.com>
2022-09-13 08:33:27 +0700

    pass isOpenSidebar to sidebar

--- src/App.vue
+++ src/App.vue
@@ -1,69 +1,10 @@
 <template>
   <v-app>
-    <v-app-bar color="white" elevation="0" dark>
-      <v-row no-gutters align="center">
-        <v-col xs="12" sm="12" md="4" lg="2" class="custom-col">
-          <div class="d-flex align-center">
-            <v-img
-              alt="Vuetify Logo"
-              class="shrink mr-2"
-              contain
-              src="https://technext.github.io/elaadmin/images/logo.png"
-              transition="transition"
-              width="160"
-            />
-            <v-app-bar-nav-icon
-              color="#99abb4"
-              @click.stop="handleDrawer"
-            ></v-app-bar-nav-icon>
+    <Header
+      @eventToggleMenuDrawer="toggleMenuDrawer"
+      @eventToggleSidebar="toggleSidebar"
+    />
 
-            <v-app-bar-nav-icon
-              color="#99abb4"
-              @click="toggleSidebar"
-            ></v-app-bar-nav-icon>
-          </div>
-        </v-col>
-        <v-col xs="12" sm="12" md="8" lg="10" class="custom-col">
-          <v-row
-            align="center"
-            justify="end"
-            flat
-            tile
-            style="flex-wrap: nowrap"
-          >
-            <v-btn icon color="#99abb4">
-              <v-icon>mdi-magnify</v-icon>
-            </v-btn>
-            <v-badge color="red" content="6" offset-x="20" offset-y="25">
-              <v-btn icon color="#99abb4">
-                <v-icon>mdi-bell</v-icon>
-              </v-btn>
-            </v-badge>
-            <v-badge color="blue" content="6" offset-x="20" offset-y="25">
-              <v-btn icon color="#99abb4">
-                <v-icon>mdi-email</v-icon>
-              </v-btn>
-            </v-badge>
-
-            <v-badge
-              bordered
-              bottom
-              color="green accent-4"
-              dot
-              offset-x="10"
-              offset-y="10"
-              style="margin-right: 20px; margin-left: 10px"
-            >
-              <v-avatar size="40">
-                <v-img
-                  src="https://cdn.vuetifyjs.com/images/lists/2.jpg"
-                ></v-img>
-              </v-avatar>
-            </v-badge>
-          </v-row>
-        </v-col>
-      </v-row>
-    </v-app-bar>
     <div class="left-side-bar">
       <LeftSideBar :isOpenSidebar="isOpenSidebar" />
     </div>
@@ -78,6 +19,7 @@
 <script>
 import LeftMenuDrawer from "./components/LeftMenu/LeftMenuDrawer.vue";
 import LeftSideBar from "./components/LeftMenu/LeftSideBar.vue";
+import Header from "./components/Header/Header.vue";
 export default {
   name: "App",
   data: () => ({
@@ -85,17 +27,18 @@ export default {
     group: null,
     isOpenSidebar: false,
   }),
-  components: { LeftMenuDrawer, LeftSideBar },
+  components: { LeftMenuDrawer, LeftSideBar, Header },
   methods: {
     setDrawer(val) {
       this.drawer = val;
     },
-    handleDrawer() {
-      this.drawer = !this.drawer;
-    },
+
     toggleSidebar() {
       console.log("toggle sidebar");
       this.isOpenSidebar = !this.isOpenSidebar;
+    },
+    toggleMenuDrawer() {
+      this.drawer = !this.drawer;
     },
   },
 };
---
+++ src/components/Header/Header.vue
@@ -0,0 +1,83 @@
+<template>
+  <v-app-bar color="white" elevation="0" dark>
+    <v-row no-gutters align="center">
+      <v-col xs="12" sm="12" md="4" lg="2" class="custom-col">
+        <div class="d-flex align-center">
+          <v-img
+            alt="Vuetify Logo"
+            class="shrink mr-2"
+            contain
+            src="https://technext.github.io/elaadmin/images/logo.png"
+            transition="transition"
+            width="160"
+          />
+          <v-app-bar-nav-icon
+            color="#99abb4"
+            @click.stop="handleDrawer"
+          ></v-app-bar-nav-icon>
+
+          <v-app-bar-nav-icon
+            color="#99abb4"
+            @click="toggleSidebar"
+          ></v-app-bar-nav-icon>
+        </div>
+      </v-col>
+      <v-col xs="12" sm="12" md="8" lg="10" class="custom-col">
+        <v-row align="center" justify="end" flat tile style="flex-wrap: nowrap">
+          <v-btn icon color="#99abb4">
+            <v-icon>mdi-magnify</v-icon>
+          </v-btn>
+          <v-badge color="red" content="6" offset-x="20" offset-y="25">
+            <v-btn icon color="#99abb4">
+              <v-icon>mdi-bell</v-icon>
+            </v-btn>
+          </v-badge>
+          <v-badge color="blue" content="6" offset-x="20" offset-y="25">
+            <v-btn icon color="#99abb4">
+              <v-icon>mdi-email</v-icon>
+            </v-btn>
+          </v-badge>
+
+          <v-badge
+            bordered
+            bottom
+            color="green accent-4"
+            dot
+            offset-x="10"
+            offset-y="10"
+            style="margin-right: 20px; margin-left: 10px"
+          >
+            <v-avatar size="40">
+              <v-img src="https://cdn.vuetifyjs.com/images/lists/2.jpg"></v-img>
+            </v-avatar>
+          </v-badge>
+        </v-row>
+      </v-col>
+    </v-row>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  methods: {
+    // setDrawer(val) {
+    //   this.drawer = val;
+    // },
+    handleDrawer() {
+      console.log("handleDrawer");
+      this.$emit("eventToggleMenuDrawer");
+
+      //   this.drawer = !this.drawer;
+    },
+    toggleSidebar() {
+      console.log("toggle sidebar");
+      this.$emit("eventToggleSidebar");
+
+      //   this.isOpenSidebar = !this.isOpenSidebar;
+    },
+  },
+};
+</script>
+
+<style></style>

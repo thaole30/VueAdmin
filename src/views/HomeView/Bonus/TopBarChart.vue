bacceee5d6dc3fc8b85bb99fe143b34f57177637
thaole30 <thaolee3005@gmail.com>
2022-09-26 17:02:29 +0700

    done mixed chart

---
+++ src/views/HomeView/Bonus/TopBarChart.vue
@@ -0,0 +1,130 @@
+<template>
+  <div class="bar-cart">
+    <Bar
+      :chart-options="chartOptions"
+      :chart-data="chartData"
+      :chart-id="chartId"
+      :dataset-id-key="datasetIdKey"
+      :plugins="plugins"
+      :css-classes="cssClasses"
+      :styles="styles"
+      :width="width"
+      :height="height"
+    />
+  </div>
+</template>
+
+<script>
+import { Bar } from "vue-chartjs/legacy";
+
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+  CategoryScale,
+  LinearScale,
+} from "chart.js";
+
+ChartJS.register(
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+  CategoryScale,
+  LinearScale
+);
+
+export default {
+  name: "TopBarChart",
+  components: {
+    Bar,
+  },
+  props: {
+    chartId: {
+      type: String,
+      default: "bar-chart",
+    },
+    datasetIdKey: {
+      type: String,
+      default: "label",
+    },
+    width: {
+      type: Number,
+      default: 400,
+    },
+    height: {
+      type: Number,
+      default: 150,
+    },
+    cssClasses: {
+      default: "",
+      type: String,
+    },
+    styles: {
+      type: Object,
+      default: () => {},
+    },
+    plugins: {
+      type: Array,
+      default: () => [],
+    },
+  },
+  data() {
+    return {
+      chartData: {
+        labels: [
+          "January",
+          "February",
+          "March",
+          "April",
+          "May",
+          "June",
+          "July",
+          "August",
+          "September",
+          "October",
+          "November",
+          "December",
+        ],
+        datasets: [
+          {
+            label: "Data One",
+            backgroundColor: "#D8CAF3",
+            data: [18, 25, 12, 19, 23, 35, 26, 12, 17, 22],
+          },
+        ],
+      },
+      chartOptions: {
+        responsive: true,
+        maintainAspectRatio: false,
+
+        scales: {
+          x: {
+            grid: {
+              drawBorder: false,
+              display: false,
+            },
+            display: false,
+          },
+          y: {
+            grid: {
+              display: false,
+
+              drawBorder: false,
+            },
+            display: false,
+          },
+        },
+
+        plugins: {
+          legend: {
+            display: false,
+          },
+        },
+      },
+    };
+  },
+};
+</script>
